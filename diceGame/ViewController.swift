225f58de171a7ce20b0ab217337249d6fc435b83
P-DING <pding2048@gmail.com>
2023-07-31 12:29:34 +0900

    커밋

--- diceGame/ViewController.swift
+++ diceGame/ViewController.swift
@@ -1,19 +1,32 @@
-//
-//  ViewController.swift
-//  diceGame
-//
-//  Created by PDing ㅤ on 2023/07/31.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstDiceImage: UIImageView!
+    @IBOutlet weak var secondDiceImage: UIImageView!
+    
+    
+    var diceArrays = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        firstDiceImage.image = diceArrays.randomElement()
+        secondDiceImage.image = diceArrays.randomElement()
+        
+        
     }
-
-
+    
+    @IBAction func button(_ sender: UIButton) {
+        
+        firstDiceImage.image = diceArrays.randomElement()
+        secondDiceImage.image = diceArrays.randomElement()
+        
+        
+    }
+    
+    
 }
 
