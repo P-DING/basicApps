4742a8e8c36352b6dcdfb5f4fc7d4eb7b5227982
P-DING <pding2048@gmail.com>
2023-07-31 18:25:59 +0900

    1

---
+++ rpsGame/rpsGame/Rps.swift
@@ -0,0 +1,14 @@
+//
+//  Rps.swift
+//  rpsGame
+//
+//  Created by PDing ㅤ on 2023/07/31.
+//
+
+import Foundation
+// 원시값(raw value)
+enum Rps: Int {
+    case rock
+    case paper
+    case scissors
+}
--- rpsGame/rpsGame/ViewController.swift
+++ rpsGame/rpsGame/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rpsResult: UILabel!
+    
+    @IBOutlet weak var comChoiceImage: UIImageView!
+    @IBOutlet weak var myChoiceImage: UIImageView!
+    
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
+        comChoiceImage.image = #imageLiteral(resourceName: "ready")
+        myChoiceImage.image = #imageLiteral(resourceName: "ready")
+        
+        
     }
 
+    @IBAction func rpsButton(_ sender: UIButton) {
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func rpsSelect(_ sender: UIButton) {
+        
+        // 컴퓨터와 나의 값을 비교
+        // 이겼다 졌다 비겼다 나오게 하기
+        
+    }
+    
+    @IBAction func rpsReset(_ sender: UIButton) {
+        
+        // 선택하세요 표시
+        // 준비 표시
+        // 이미지 처음으로 초기화
+        // 값 초기화
+        
+    }
+    
+    
+    
+    
 
 }
 
