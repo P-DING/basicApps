fea05ea3e6160f9e7699e53fa7506865ec46373d
P-DING <pding2048@gmail.com>
2023-07-31 13:01:29 +0900

    커밋

---
+++ diceGame/diceGame/ViewController.swift
@@ -0,0 +1,34 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstDiceImage: UIImageView!
+    @IBOutlet weak var secondDiceImage: UIImageView!
+    
+    
+    var diceArrays = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        firstDiceImage.image = diceArrays.randomElement()
+        secondDiceImage.image = diceArrays.randomElement()
+        
+        
+    }
+    
+    @IBAction func button(_ sender: UIButton) {
+        
+        firstDiceImage.image = diceArrays.randomElement()
+        secondDiceImage.image = diceArrays.randomElement()
+        
+        
+    }
+
+    
+}
+ 
+
+ 
